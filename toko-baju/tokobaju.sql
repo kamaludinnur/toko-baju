349d13bd17b9f8b5741f15c6935edf4cb182366e
muhammad.abrari <muhammad.abrari@32030872-a922-c14d-b167-f0f783761471>
2010-12-24 23:19:16 +0000

    Commit pertama habis reset. Cuma ada base CodeIgniter sama SQL databasenya.

---
+++ toko-baju/tokobaju.sql
@@ -0,0 +1,142 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE IF NOT EXISTS `agen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `hp` varchar(50) NOT NULL,
+  `alamat` tinytext NOT NULL,
+  `diskon` decimal(10,0) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nama` (`nama`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+CREATE TABLE IF NOT EXISTS `merek` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) DEFAULT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+
+CREATE TABLE IF NOT EXISTS `model` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) NOT NULL,
+  `merek` int(11) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+
+CREATE TABLE IF NOT EXISTS `produk` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` int(11) NOT NULL,
+  `ukuran` int(11) NOT NULL,
+  `warna` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga_beli` decimal(10,0) NOT NULL,
+  `harga_jual` decimal(10,0) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=140 ;
+
+
+CREATE TABLE IF NOT EXISTS `record_stok` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` datetime NOT NULL,
+  `produk` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `stok_akhir` int(11) NOT NULL,
+  `jenis` enum('konsumen','agen','tambah','retur_agen','retur_konsumen','retur_pabrik','reject_agen','reject_konsumen','reject_pabrik','kehilangan') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=111 ;
+
+
+CREATE TABLE IF NOT EXISTS `setting` (
+  `item` varchar(255) NOT NULL,
+  `value` tinytext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `setting` (`item`, `value`) VALUES
+('username_admin', ''),
+('password_admin', '');
+
+CREATE TABLE IF NOT EXISTS `transaksi_agen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` datetime NOT NULL,
+  `agen` int(11) NOT NULL,
+  `produk` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `keuntungan` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+
+
+CREATE TABLE IF NOT EXISTS `transaksi_kehilangan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produk` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` decimal(11,0) NOT NULL,
+  `agen` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Kalo konsumen, agen = 0, selainnya pake ID agen' AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `transaksi_konsumen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` datetime NOT NULL,
+  `produk` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `keuntungan` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+
+CREATE TABLE IF NOT EXISTS `transaksi_reject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produk` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `agen` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Kalo konsumen, agen = 0, selainnya pake ID agen' AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `transaksi_retur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produk` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `agen` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Kalo konsumen, agen = 0, selainnya pake ID agen' AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `ukuran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(10) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `warna` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
